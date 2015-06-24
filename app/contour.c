b0482135656b0057da9e64ae61ca58a5b753f84a
DodgySmalls <marten.wiebe@gmail.com>
2015-06-23 20:35:29 -0700

    rebuilding clean

---
+++ app/contour.c
@@ -0,0 +1,61 @@
+/*
+ * This file is part of the OpenKinect Project. http://www.openkinect.org
+ *
+ * Copyright (c) 2010 individual OpenKinect contributors. See the CONTRIB file
+ * for details.
+ *
+ * This code is licensed to you under the terms of the Apache License, version
+ * 2.0, or, at your option, the terms of the GNU General Public License,
+ * version 2.0. See the APACHE20 and GPL2 files for the text of the licenses,
+ * or the following URLs:
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.gnu.org/licenses/gpl-2.0.txt
+ *
+ * If you redistribute this file in source form, modified or unmodified, you
+ * may:
+ *   1) Leave this header intact and distribute it under the same terms,
+ *      accompanying it with the APACHE20 and GPL20 files, or
+ *   2) Delete the Apache 2.0 clause and accompany it with the GPL2 file, or
+ *   3) Delete the GPL v2 clause and accompany it with the APACHE20 file
+ * In all cases you must keep the copyright notice intact and include a copy
+ * of the CONTRIB file.
+ *
+ * Binary distributions must follow the binary distribution requirements of
+ * either License.
+ */
+
+//inline defines simpler than arguments with cmake
+#define GLOBAL_DEBUG
+#define GL_DEBUG
+
+#ifdef GLOBAL_DEBUG
+#include assert.h
+#include <inttypes.h>
+#endif
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "libfreenect.h"
+
+#include <pthread.h>
+
+#if defined(__APPLE__)
+#include <GLUT/glut.h>
+#else
+#include <GL/glut.h>
+#endif
+
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+/**
+		function headers
+**/
+struct colour8_t convert_depth_to_colour(uint16_t);
+/**-----------------------------------**/
+
+//A glob representing a pixel colour
+struct colour8_t {
+	uint8_t RGB[3];
+};
